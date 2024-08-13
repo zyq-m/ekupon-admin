ee7ae679ebef7677a1b5108f6209acd171d05f7e
haziq <haziq.musa02@gmail.com>
2024-08-14 01:07:24 +0800

    add set date filter

--- src/pages/user-data/transaction/cafe.jsx
+++ src/pages/user-data/transaction/cafe.jsx
@@ -15,11 +15,14 @@ export default function CafeTransaction() {
   const [transaction, setTransaction] = useState([]);
   const [cafeName, setCafeName] = useState("");
   const [select, setSelect] = useState("");
+  const [date, setDate] = useState({});
   const { error, showModal } = useModal();
 
   useEffect(() => {
     api
-      .get(`/admin/cafe/transactions/${id}`, { params: { fundType: select } })
+      .get(`/admin/cafe/transactions/${id}`, {
+        params: { fundType: select, from: date?.from, to: date?.to },
+      })
       .then((res) => {
         setTransaction(res.data);
         setCafeName(res.data.data[0].transaction.cafe.name);
@@ -27,11 +30,16 @@ export default function CafeTransaction() {
       .catch((err) => {
         showModal(err.response.data.message);
       });
-  }, [id, select]);
+  }, [id, select, date?.from, date?.to]);
 
   return (
     <Layout title={cafeName}>
-      <Tablefilter fundType={true} setSelect={(e) => setSelect(e)} />
+      <Tablefilter
+        fundType={true}
+        date={true}
+        setSelect={(e) => setSelect(e)}
+        setDate={(e) => setDate(e)}
+      />
       <TransactionTable cafe={true} data={transaction} />
       <Modal error={error} />
     </Layout>
