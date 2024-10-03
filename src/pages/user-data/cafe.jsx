3a4f7fc41c71e7579e4f6f1b6b547bef88f9a804
haziq <haziq.musa02@gmail.com>
2024-10-03 16:18:46 +0800

    add qr

--- src/pages/user-data/cafe.jsx
+++ src/pages/user-data/cafe.jsx
@@ -4,70 +4,99 @@ import { Layout, BtnSuspend, Loading } from "../../components";
 
 import { api } from "../../services/axios";
 
+import FileDownloadOutlinedIcon from "@mui/icons-material/FileDownloadOutlined";
+
 export default function CafeData() {
-  const [cafe, setCafe] = useState([]);
+    const [cafe, setCafe] = useState([]);
 
-  useEffect(() => {
-    api
-      .get("/admin/cafe")
-      .then((res) => {
-        setCafe(res.data.cafe);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }, []);
+    const downloadQR = async () => {
+        try {
+            const res = await api.get("/admin/qr", { responseType: "blob" });
 
-  if (!cafe.length) {
+            const url = window.URL.createObjectURL(new Blob([res.data]));
+            const link = document.createElement("a");
+
+            link.href = url;
+            link.setAttribute("download", "Cafe_QR_Code.zip");
+            document.body.appendChild(link);
+            link.click();
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        api.get("/admin/cafe")
+            .then((res) => {
+                setCafe(res.data.cafe);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }, []);
+
+    if (!cafe.length) {
+        return (
+            <Layout title="Cafe data">
+                <Loading />
+            </Layout>
+        );
+    }
+
     return (
-      <Layout title="Cafe data">
-        <Loading />
-      </Layout>
+        <Layout title="Cafe data">
+            <div className="overflow-x-auto">
+                <div className="flex justify-end">
+                    <button
+                        className="btn btn-sm btn-warning"
+                        onClick={downloadQR}
+                    >
+                        <FileDownloadOutlinedIcon />
+                        Download QR
+                    </button>
+                </div>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Cafe Name</th>
+                            <th>Owner</th>
+                            <th>Account No.</th>
+                            <th>Bank Name</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {cafe?.map((d, i) => {
+                            return (
+                                <>
+                                    <tr className="hover" key={d.id}>
+                                        <th>{i + 1}</th>
+                                        <td>{d.name}</td>
+                                        <td>{d.user.profile.name}</td>
+                                        <td>{d.accountNo || "N/A"}</td>
+                                        <td>{d.bank || "N/A"}</td>
+                                        <td>
+                                            <BtnSuspend
+                                                active={d.user.active}
+                                                userId={d.userId}
+                                            />
+                                        </td>
+                                        <td>
+                                            <Link
+                                                to={`transaction/${d.id}`}
+                                                className="btn btn-ghost btn-xs"
+                                            >
+                                                transactions
+                                            </Link>
+                                        </td>
+                                    </tr>
+                                </>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        </Layout>
     );
-  }
-
-  return (
-    <Layout title="Cafe data">
-      <div className="overflow-x-auto">
-        <table className="table">
-          <thead>
-            <tr>
-              <th></th>
-              <th>Cafe Name</th>
-              <th>Owner</th>
-              <th>Account No.</th>
-              <th>Bank Name</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cafe?.map((d, i) => {
-              return (
-                <>
-                  <tr className="hover" key={d.id}>
-                    <th>{i + 1}</th>
-                    <td>{d.name}</td>
-                    <td>{d.user.profile.name}</td>
-                    <td>{d.accountNo || "N/A"}</td>
-                    <td>{d.bank || "N/A"}</td>
-                    <td>
-                      <BtnSuspend active={d.user.active} userId={d.userId} />
-                    </td>
-                    <td>
-                      <Link
-                        to={`transaction/${d.id}`}
-                        className="btn btn-ghost btn-xs"
-                      >
-                        transactions
-                      </Link>
-                    </td>
-                  </tr>
-                </>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-    </Layout>
-  );
 }
